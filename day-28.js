d16de866cbaf024b37b92f42b7cbee0f54d89c7f
Roman <57969831+Roman-Krytskyi@users.noreply.github.com>
2020-08-16 19:25:17 +0300

    Update day-28.js

--- day-28.js
+++ day-28.js
@@ -1,4 +1,11 @@
 // http://www.codewars.com/kata/a-function-within-a-function
 const always = (n) => () => n;
 
-// 
+// https://www.codewars.com/kata/singleton-pattern/train/javascript
+var Singleton = (function(){
+  let instance;
+  
+  return function () {
+    return instance || (instance = this)
+  }
+}) ();
