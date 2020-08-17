f37f9f7bf17bdfa190743788ee1d91a99a633808
Roman <57969831+Roman-Krytskyi@users.noreply.github.com>
2020-08-17 21:26:24 +0300

    Create day-29.js

---
+++ day-29.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/how-new-works
+var myObj = {};
+myObj.__proto__ = MyObject.prototype;
+MyObject.call(myObj);
+
+// http://www.codewars.com/kata/replicate-new
+// coming soon :)
